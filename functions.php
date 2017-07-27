cbe2a87ab48edf57cb31c31144c14983e59af485
Hugh Brock <hbrock@gmail.com>
2017-07-27 08:20:42 +0200

    Remove the password strength javascript from the login and checkout pages

--- functions.php
+++ functions.php
@@ -12,3 +12,13 @@ function custom_variation_price( $price, $product ) {
      return $price;
 
 }
+
+/**
+ * disable the password strength check
+ **/
+function wc_ninja_remove_password_strength() {
+	if ( wp_script_is( 'wc-password-strength-meter', 'enqueued' ) ) {
+		wp_dequeue_script( 'wc-password-strength-meter' );
+	}
+}
+add_action( 'wp_print_scripts', 'wc_ninja_remove_password_strength', 100 );
